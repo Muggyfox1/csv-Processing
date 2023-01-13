cf5eafe9102eacbe137e5182421715e41a60ee02
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2023-01-13 16:12:19 -0500

    exports to CSV now

--- main.js
+++ main.js
@@ -13,8 +13,10 @@ let enviroment = {
     _loading: false,
     downloadList:""
 }
-let inputs = {
-    CSVObject:[]
+let data = {
+    CSVObject:[],
+    shuffledCSV:"",
+    origionalCSV:""
 }
 
 const fileSelector = document.getElementById('file-input');
@@ -24,21 +26,42 @@ fileSelector.addEventListener('change', (event) => {
     const file = event.target.files[0];
     file.text()
     .then((csvString) => {
-        processCSV(csvString);
+        localStorage.setItem("valueSource", csvString)
+        data.origionalCSV = csvString
         enviroment.loading = false;
     });
 });
 
 const durationInput = document.getElementById('duration');
-
 const loadingBar = document.getElementById('loading-bar');
 const generateButton = document.getElementById('generate-button');
 const previewList = document.getElementById("list");
 
-function processCSV(csvString){
-    inputs.CSVObject = csvString.split("\r\n");
-    inputs.CSVObject.slice(0,1);
-    inputs.CSVObject = inputs.CSVObject.map(line => {
+function submit(){
+    enviroment.loading = true;
+    shuffleList();
+    processCSV();
+    createListPreview();
+    createNewFile();
+    enviroment.loading = false;
+}
+
+function isLoading(){
+    loadingBar.classList.remove("hidden");
+    generateButton.disabled = true;
+}
+
+function doneLoading(){
+    loadingBar.classList.add("hidden");
+    if(data.origionalCSV.length > 0){
+        generateButton.disabled = false;
+    }
+}
+
+function processCSV(){
+    data.CSVObject = data.shuffledCSV.split("\r\n");
+    data.CSVObject.slice(0,1);
+    data.CSVObject = data.CSVObject.map(line => {
         let lineSplits = line.split(";")
         if (lineSplits.length < 3){
             return;
@@ -56,71 +79,51 @@ function processCSV(csvString){
     .filter(f => f != undefined)
     .slice(1);
     
-    localStorage.setItem("list", inputs.CSVObject);
-}
-
-function submit(){
-    enviroment.loading = true;
-    shuffleList();
-    createListPreview();
-    createNewFile();
-    enviroment.loading = false;
-}
-
-function isLoading(){
-    loadingBar.classList.remove("hidden");
-    generateButton.disabled = true;
-}
-
-function doneLoading(){
-    loadingBar.classList.add("hidden");
-    if(inputs.CSVObject.length > 0){
-        generateButton.disabled = false;
-    }
+    localStorage.setItem("list", data.CSVObject);
 }
 
 function createNewFile(){
-    let CSVObjRef = inputs.CSVObject.slice(0);
-    let textArray = [];
+    let csvStringRef = data.shuffledCSV.slice(0)
+    .split("\r\n")
+    .filter(l => l.length > 0);
+    let headers = csvStringRef.shift()
+    headers = "Day;" + headers;
+    csvStringRef = csvStringRef.map(line => ";" + line);
+
     let dayCount = 0;
-    let today = new Date();
-    while(CSVObjRef.length > 0){
-        let familiesPerDay = Math.floor(CSVObjRef.length / (durationInput.value - dayCount));
+    let textArray = [];
+    while(csvStringRef.length > 0){
+        let familiesPerDay = Math.floor(csvStringRef.length / (durationInput.value - dayCount));
 
-        dayText = "Day: " + (dayCount + 1) + ", "
-        + (today.getMonth() + 1) + "/" + today.getDate();
-        textArray.push(dayText);
-
+        let dayText = "Day: " + (dayCount + 1);
         dayCount++;
-        today.setDate(today.getDate() + 1)
 
-        for (let i = 0; i < familiesPerDay && CSVObjRef.length > 0 || 
-            dayCount == durationInput.value && CSVObjRef.length > 0; i++) {
+        for (let i = 0; i < familiesPerDay && csvStringRef.length > 0 || 
+            dayCount == durationInput.value && csvStringRef.length > 0; i++) {
             
-            let familyFromCSV = CSVObjRef.pop();
-            textArray.push("    " + familyFromCSV.lastName + ":");
-            
-            familyFromCSV.people.forEach(personName => {
-                textArray.push("        " + personName.trim());
-            });
-
-            textArray.push("")
+            let familyFromCSV = csvStringRef.pop();
+            textArray.push(dayText + familyFromCSV)
+            dayText = "";
         }
     }
 
+    textArray.unshift(headers);
     enviroment.downloadList = textArray.join("\n");
     localStorage.setItem("listForDownload", enviroment.downloadList);
-
-    createDownloadLink("List.txt", enviroment.downloadList)
+    createDownloadLink("List.csv", enviroment.downloadList)
 }
 
 function shuffleList(){
+    let splitCSV = data.origionalCSV.slice(0).split("\r\n");
+    let headers = splitCSV.shift();
+
     let newList = [];
-    while(inputs.CSVObject.length > 0){
-        let index = getRandomInt(inputs.CSVObject.length);
-        newList.push(inputs.CSVObject.splice(index ,1)[0]);
+    while(splitCSV.length > 0){
+        let index = getRandomInt(data.CSVObject.length);
+        newList.push(splitCSV.splice(index ,1)[0]);
     }
-    inputs.CSVObject = newList;
+    newList.unshift(headers);
+    data.shuffledCSV = newList.join("\r\n");
 }
 
 function getRandomInt(max) {
@@ -129,16 +132,15 @@ function getRandomInt(max) {
 
 function createListPreview(){
     previewList.innerHTML = "";
-    let CSVObjRef = inputs.CSVObject.slice(0);
+    let CSVObjRef = data.CSVObject.slice(0);
 
     let dayCount = 0;
     let today = new Date();
     while(CSVObjRef.length > 0){
         let familiesPerDay = Math.floor(CSVObjRef.length / (durationInput.value - dayCount));
 
-        let day = document.createElement("li");
-        day.innerText = "Day: " + (dayCount + 1) + ", "
-        + (today.getMonth() + 1) + "/" + today.getDate();
+        let day = document.createElement("div");
+        day.innerText = "Day: " + (dayCount + 1) + ", ";
         
         dayCount++;
         today.setDate(today.getDate() + 1)
@@ -169,7 +171,6 @@ function createListPreview(){
     localStorage.setItem("previewList", previewList.innerHTML);
 }
 
-
 function createDownloadLink(filename, text) {
     var element = document.getElementById('download');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -179,6 +180,12 @@ function createDownloadLink(filename, text) {
 
 function loadLocalStorage(){
     enviroment.loading = true;
+    let csvString = localStorage.getItem("valueSource");
+    if(csvString){
+        data.origionalCSV = csvString;
+        processCSV(csvString);
+    }
+    
     let previewListInnerHTML = localStorage.getItem("previewList"); 
     if(previewListInnerHTML){
         previewList.innerHTML = previewListInnerHTML;
